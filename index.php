b5cb2fb993cb754ff097cbf401ef03073353aafc
Bemoty <signup.bemoty@outlook.com>
2018-08-19 20:53:54 +0200

    rearranged folder structure

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
-  <link rel="stylesheet" href="assets/style.css"/>
+  <link rel="stylesheet" href="assets/css/style.css"/>
   <?php
-  $conn = new mysqli("localhost", "USERNAME", "PASSWORD", "TABLE");
+  $conn = new mysqli("localhost", "bemoty_web", "bemoty_web", "bemoty_mojira");
   $conn->set_charset('utf8mb4');
 
   if ($conn->connect_error) {
@@ -82,6 +82,6 @@
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="assets/custom.js"></script>
+  <script src="assets/js/custom.js"></script>
 </body>
 </html>
